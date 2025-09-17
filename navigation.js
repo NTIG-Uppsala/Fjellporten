52cbaaa5cae8229638ca9da7ea32934e30b47751
DaPakoREE <dapakoreeee@gmail.com>
2025-09-17 13:43:14 +0200

    Clarified navigation.js

--- navigation.js
+++ navigation.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const pageSelect = document.getElementById("pageSelect");
-    if (!pageSelect) return;
 
-    const current = window.location.pathname.split("/").pop();
-    for (let i = 0; i < pageSelect.options.length; i++) {
-        if (pageSelect.options[i].value === current) {
-            pageSelect.selectedIndex = i;
+    // Finds an element with the ID "pageSelect" and if none exist, it returns null
+    const PAGE_SELECT = document.getElementById("pageSelect");
+    if (!PAGE_SELECT) return;
+
+    // Gets the last index of a list containing all the separate parts of the current pages filepath
+    const CURRENT = window.location.pathname.split("/").pop();
+    
+    // Loops through all options in the dropdown menu until it matches the current pages filename
+    for (let i = 0; i < PAGE_SELECT.options.length; i++) {
+        if (PAGE_SELECT.options[i].value === CURRENT) {
+
+            // Sets the selected option to the one that matches the current pages filename
+            PAGE_SELECT.selectedIndex = i;
             break;
         }
     }
 
-    pageSelect.addEventListener("change", (e) => {
-        const url = e.target.value;
-        if (!url) return;
-        window.location.href = url;
+    // Listens if the selected dropdown menu option changes
+    PAGE_SELECT.addEventListener("change", (e) => {
+
+        // Redirects the user to the selected page
+        const URL = e.target.value;
+        if (!URL) return;
+        window.location.href = URL;
     });
 });
